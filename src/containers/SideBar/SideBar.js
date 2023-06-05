54ea647b8f3262cbc56f824fe80e626cd2969ea1
57073731sb <begzodsirojiddinov9@gmail.com>
2023-06-05 12:08:40 +0500

    changed components

--- src/containers/SideBar/SideBar.js
+++ src/containers/SideBar/SideBar.js
@@ -7,28 +7,54 @@ import {Subscriptions} from './Subscriptions/Subscriptions';
 import {SideBarFooter} from './SideBarFooter/SideBarFooter';
 
 export class SideBar extends React.Component {
-  render() {
-    return (
-      <Menu borderless vertical stackable fixed='left' className='side-nav'>
-        <SideBarItem path='/' label='Home' icon='home'/>
-        <SideBarItem path='/feed/trending' label='Trending' icon='fire'/>
-        <SideBarItem label='Followers' icon='spy'/>
-        <Divider/>
-        <SideBarHeader title='Library'/>
-        <SideBarItem label='History' icon='history'/>
-        <SideBarItem label='Watch later' icon='clock'/>
-        <SideBarItem label='Liked videos' icon='thumbs up'/>
-        <Divider/>
-        <Subscriptions/>
-        <SideBarHeader title='More from Youtube'/>
-        <SideBarItem label='Movies and Shows' icon='film'/>
-        <Divider/>
-        <SideBarItem label='Report history' icon='flag'/>
-        <SideBarItem label='Help' icon='help circle'/>
-        <SideBarItem label='Send feedback' icon='comment'/>
-        <Divider/>
-        <SideBarFooter/>
-      </Menu>
-    );
+  constructor(props) {
+    super(props);
+    this.state = {
+      windowWidth: window.innerWidth
+    };
+  }
+
+  componentDidMount() {
+    window.addEventListener('resize', this.handleResize);
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('resize', this.handleResize);
+  }
+
+  handleResize = () => {
+    this.setState({ windowWidth: window.innerWidth });
+  };
+
+    render() {
+    const { windowWidth } = this.state;
+    if(windowWidth > 700){
+      return (
+        <Menu borderless vertical stackable fixed='left' className='side-nav'>
+          <SideBarItem path='/' label='Home' icon='home'/>
+          <SideBarItem path='/feed/trending' label='Trending' icon='fire'/>
+          <SideBarItem label='Followers' icon='spy'/>
+          <Divider/>
+          <SideBarHeader title='Library'/>
+          <SideBarItem label='History' icon='history'/>
+          <SideBarItem label='Watch later' icon='clock'/>
+          <SideBarItem label='Liked videos' icon='thumbs up'/>
+          <Divider/>
+          <Subscriptions/>
+          <SideBarHeader title='More from Youtube'/>
+          <SideBarItem label='Movies and Shows' icon='film'/>
+          <Divider/>
+          <SideBarItem label='Report history' icon='flag'/>
+          <SideBarItem label='Help' icon='help circle'/>
+          <SideBarItem label='Send feedback' icon='comment'/>
+          <Divider/>
+          <SideBarFooter/>
+        </Menu>
+      );
+    } else {
+      return(
+        <></>
+      )
+    }
   }
 }
